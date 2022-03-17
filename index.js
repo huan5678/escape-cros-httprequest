0e5e9cfdcd83198a8abb365cf006fd28561d1c7d
huan5678 <huan5678@gmail.com>
2022-03-17 17:46:33 +0800

    [upgrade] cros + 內頁

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ console.clear()
 
 moment.locale('zh-tw')
 
+const crosUrl = 'https://esc-cros-anywhere.herokuapp.com'
+
 const baseUrl = 'https://hex-escape-room.herokuapp.com'
 const apiPath = '/api/cors/news'
 
 const list = document.querySelector('.list')
-
-const btn = document.querySelector('.btn')
 
 const page = document.querySelector('.col-12')
 
@@ -23,7 +23,7 @@ const render = function(data){
               <div class="card bg-dark text-white card-gradient" ><img src="${data?.urlToImage}" class="card-img" alt="${data?.title}">
                 <div class="card-img-overlay">
                   <h5 class="card-title">${data?.title}</h5>
-                  <p class="card-text">${moment(data?.publishedAt).format('YYYY-MM-DD HH:mm:ss')}</p><a href="#" class="stretched-link" data-id="${data?.id}"></a>
+                  <p class="card-text">${moment(data?.publishedAt).format('YYYY-MM-DD Ahh:mm:ss')}</p><a href="#" class="stretched-link" data-id="${data?.id}"></a>
                 </div>
               </div>
             </div>
@@ -32,45 +32,58 @@ const render = function(data){
   
 }
 
-const getAllNews = function(){
-   axios.get(`${baseUrl}${apiPath}`).then(res=>{
-  console.log(res.data)
-     res.data.data.forEach(item=>{
-       render(item)
-     })
-     list.innerHTML = result
-     const cards = document.querySelectorAll('.card-gradient')
-  cards.forEach(card => {
-    card.addEventListener('click', ()=>{
-  console.log(event.target.dataset.id)
-  isOpen = true
-  getNews(event.target.dataset.id)
-  render()
-})
-  console.log(card)
-  })
-})
-  .catch(err => console.error(err))
+const renderPage = function (data) {
+  (isOpen) ? page.classList.remove("hidden") : page.classList.add('hidden');
+  const template = `
+  <div class="sticky-top">
+        <!-- 點擊關閉側欄 -->
+        <button class="btn btn-outline-secondary shadow border-0">
+          <i class="bi bi-caret-left-fill"></i> 返回
+        </button>
+        <h2 class="mt-3">${data?.title}</h2>
+        <span>${moment(data?.publishedAt).format('YYYY-MM-DD Ahh:mm:ss')}</span>
+        <img src="${data?.urlImage}" class="img-fluid" alt="">
+        <p>${data?.description}</p>
+        <a href="${data?.url}" target="blank">看更多</a>
+      </div>
+  `
+  page.innerHTML = template
 }
 
 const getNews = function(id = '1'){
-  axios.get(`${baseUrl}${apiPath}/:${id}`).then(res=>{
-    console.log(res.data)
-    return res.data.data
+  axios.get(`${crosUrl}/${baseUrl}${apiPath}/${id}`)
+    .then(res => {
+    // console.log(res.data)
+    renderPage(res.data.data)
+    const btn = document.querySelector('.btn')
+    btn.addEventListener('click', ()=>{
+    isOpen = false
+    render()
+  })
   })
   .catch(err => console.error(err))
 }
 
 const init = function(){
-  btn.addEventListener('click', ()=>{
-  isOpen = false
+   axios.get(`${baseUrl}${apiPath}`).then(res=>{
+  // console.log(res.data)
+     res.data.data.forEach(item=>{
+       render(item)
+     })
+     list.innerHTML = result
+  const cards = document.querySelectorAll('.card-gradient')
+  cards.forEach(card => {
+    card.addEventListener('click', ()=>{
+  // console.log(event.target.dataset.id)
+  isOpen = true
+  getNews(event.target.dataset.id)
   render()
 })
+  // console.log(card)
+  })
+})
+  .catch(err => console.error(err))
+}
 
- getAllNews();
-// getNews(1)
-  
-  
-                       }
 
-init()
+init();
